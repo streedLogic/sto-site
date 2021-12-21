080de5b7d318882b7a48f363c6749b42ffa708da
streedLogic <96439523+streedLogic@users.noreply.github.com>
2021-12-20 20:31:58 -0500

    adding styles

---
+++ styles/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  //primary
+  gray: "#EAEAEA",
+  blue: "#0764F9",
+  pink: "#E95DBF",
+  yellow: "#F7D454",
+  red: "#F53F0B",
+  black: "#242121",
+  white: "#FFFFFF",
+};
+
+export default colors;
---
+++ styles/dimensions.js
@@ -0,0 +1,9 @@
+//Dimensions for device media queries and layout padding
+const dimensions = {
+  //pixels
+  maxwidthDesktop: 1024,
+  maxwidthTablet: 768,
+  maxwidthMobile: 480,
+};
+
+export default dimensions;
